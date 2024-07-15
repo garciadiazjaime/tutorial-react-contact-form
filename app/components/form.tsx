fac79413d1dc87521689ed4cffaa033c46a588c5
Jaime <garciadiazjaime@gmail.com>
2024-07-15 09:27:05 -0500

    feat: step 3 - controlled component

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -1,4 +1,21 @@
+"use client";
+
+import { useState } from "react";
+
 export default function ContactForm() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const emailHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setEmail(event.target.value);
+  };
+
+  const messageHandler = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ): void => {
+    setMessage(event.target.value);
+  };
+
   return (
     <>
       <fieldset style={{ margin: "12px 0" }}>
@@ -9,6 +26,8 @@ export default function ContactForm() {
             fontSize: 24,
             padding: "12px 6px",
           }}
+          onChange={emailHandler}
+          value={email}
         />
       </fieldset>
       <fieldset style={{ margin: "12px 0" }}>
@@ -20,6 +39,8 @@ export default function ContactForm() {
             fontSize: 24,
             padding: "12px 6px",
           }}
+          onChange={messageHandler}
+          value={message}
         />
       </fieldset>
       <div style={{ margin: "12px 0" }}>
