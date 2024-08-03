230f420a9d4d6f44472e2ad3e91adfaee2a62542
Jaime <garciadiazjaime@gmail.com>
2024-08-03 10:03:57 -0500

    fix: one fieldset

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -19,7 +19,8 @@ export default function ContactForm() {
   return (
     <>
       <fieldset style={{ margin: "12px 0" }}>
-        <legend>* Email:</legend>
+        <legend>Say Hi</legend>
+        <label style={{ marginTop: 20, display: "block" }}>* Email:</label>
         <input
           style={{
             width: "calc(100% - 16px)",
@@ -29,9 +30,8 @@ export default function ContactForm() {
           onChange={emailHandler}
           value={email}
         />
-      </fieldset>
-      <fieldset style={{ margin: "12px 0" }}>
-        <legend>* Message:</legend>
+
+        <label style={{ marginTop: 20, display: "block" }}>* Message:</label>
         <textarea
           style={{
             width: "calc(100% - 16px)",
